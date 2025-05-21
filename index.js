28fa2ea3b8501832d9af8eba5f738f83e2f2a5a8
emmaston1err <140031388+emmaston1err@users.noreply.github.com>
2025-05-21 19:23:46 +0200

    index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ What Makes Us Different
 <:plug_arrow:1374782493491728394> • Budget-Friendly – Premium Service At Low Prices
 <:plug_arrow:1374782493491728394> • Round-The-Clock Help – Support When You Need It, Always
 <a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932>
-**:warning: Important Notice:**
+**:warning: Important Notice**
 <a:plug_impression:1374779187918803004> All payments must be made via TRC-20 cryptocurrency only. Orders are processed exclusively through ticket requests on this server. Do not trust direct messages or unofficial contacts.
 `)
       .setFooter({

a08c63b5cd366b7b3ff6a4130514a75ba6ca6fc6
emmaston1err <140031388+emmaston1err@users.noreply.github.com>
2025-05-21 19:21:42 +0200

    index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ Browse Our Selection
 <a:plug_dash:1374778714684002404> <:plug_arrow:1374782493491728394> **Shop smart – our products are secure, reliable, and proven.** <:plug_team:1374785016445276200>
 
 What Makes Us Different
-<a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932>
+<a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932>
 <:plug_arrow:1374782493491728394> • Zero Risk – 100% Safe & Verified Offers
 <:plug_arrow:1374782493491728394> • Instant Delivery – No Waiting Around
 <:plug_arrow:1374782493491728394> • Budget-Friendly – Premium Service At Low Prices

393a2c960f0331b39a2a1b0c4038c1c045677c59
emmaston1err <140031388+emmaston1err@users.noreply.github.com>
2025-05-21 18:12:38 +0200

    index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ client.once('ready', async () => {
       .setDescription(`
 Browse Our Selection
 
-> **🛡️・We offer safe, verified, and high-quality services!**
+<a:plug_dash:1374778714684002404> **🛡️・We offer safe, verified, and high-quality services!**
 > **💫・Trusted by our community — always fast & reliable.**
 
 > ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬

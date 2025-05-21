f417f44a181067eaaf41553f26a442a0f9cbc5e6
emmaston1err <140031388+emmaston1err@users.noreply.github.com>
2025-05-21 17:30:59 +0200

    index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ client.once('ready', async () => {
   if (channel && channel.type === ChannelType.GuildText) {
     const embed = new EmbedBuilder()
       .setColor(0xD391FA)
-      .setTitle('✨・Choose Your Product')
+      .setTitle(':Wa_ticket: Ticket')
       .setDescription(`
 > ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 

477000dec78a040262c607a69c07ff8bbece6a7e
emmaston1err <140031388+emmaston1err@users.noreply.github.com>
2025-05-21 19:02:59 +0200

    index.js

--- index.js
+++ index.js
@@ -71,7 +71,9 @@ Browse Our Selection
 <:plug_arrow:1374782493491728394> • Round-The-Clock Help – Support When You Need It, Always
 <a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932><a:plug_white_line:1374786910915919932>
 
-**<:warning:>・Purchases only through our [official website](https://example.com)**
+**:warning: Important Notice
+All payments must be made via TRC-20 cryptocurrency only. :currency_exchange:
+<a:plug_impression:1374779187918803004> Orders are processed exclusively through ticket requests on this server. Do not trust direct messages or unofficial contacts.**
 `)
       .setFooter({
         text: 'NitroPlug • Premium Discord Services',
